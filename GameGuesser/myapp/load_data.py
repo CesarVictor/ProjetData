e0501dd5b914a2f2cdb2de4ad8d76a4f6c364c3b
craphael <raphael.cau@ynov.com>
2024-03-29 15:15:41 -0700

    youtube

--- GameGuesser/myapp/load_data.py
+++ GameGuesser/myapp/load_data.py
@@ -1,20 +1,15 @@
-import csv
-from myapp.models import MyModel
+import pandas as pd
+import sqlite3
 
-with open('vgsales.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        _, created = MyModel.objects.get_or_create(
-            Rank=row[0],
-            Name=row[1],
-            Platform=row[2],
-            Year=row[3],
-            Genre=row[4],
-            Publisher=row[5],
-            NA_Sales=row[6],
-            EU_Sales=row[7],
-            JP_Sales=row[8],
-            Other_Sales=row[9],
-            Total_Sales=row[10],
-           
-        )
+
+df = pd.read_csv("GlobalYouTubeStatistics.csv", low_memory=False)
+
+
+conn = sqlite3.connect("youtube.db")
+
+
+df.to_sql('youtube', conn, if_exists='append', index=False)
+
+
+conn.commit()
+conn.close()
